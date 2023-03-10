6655e67e186b50789ca383d800805749a7a2addf
ryangholland <rghcode@gmail.com>
2023-03-09 19:36:49 -0500

    Add functions for basic operations

---
+++ script.js
@@ -0,0 +1,31 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function operate(operand, num1, num2) {
+  switch (operand) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+    default:
+      console.log("Operand problem");
+      return "ERROR";
+  }
+}
